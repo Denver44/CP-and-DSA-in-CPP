07d7bce630fbb4ebafb951ebb9cfa4adf4c8fdde
Durgesh <durgeshrai2012@gmail.com>
2020-09-20 12:53:28 +0530

    Codechef Probem Reverse The Number

---
+++ 12FLOW007.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int n;
+        cin >> n;
+        int rem;
+        int rev = 0;
+        while (n > 0)
+        {
+            rem = n % 10;
+            rev = (rev * 10) + rem;
+            n = n / 10;
+        }
+
+        cout << rev << endl;
+    }
+
+    return 0;
+}
