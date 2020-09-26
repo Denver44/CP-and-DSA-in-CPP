333872d59c3bc8b16798acd7ea5845cf9a484fce
Durgesh <durgeshrai2012@gmail.com>
2020-09-26 19:24:14 +0530

    Codechef Problem Mahasena

---
+++ 24AMR15A.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+    int even = 0;
+    int odd = 0;
+    cin >> size;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            even++;
+        }
+        else
+        {
+            odd++;
+        }
+    }
+    if (even > odd)
+    {
+        cout << "READY FOR BATTLE" << endl;
+    }
+    else
+    {
+        cout << "NOT READY" << endl;
+    }
+
+    return 0;
+}
