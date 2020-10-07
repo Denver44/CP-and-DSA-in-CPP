55d41b51390f5bbad1824c70f678df0ced2a814d
Durgesh <durgeshrai2012@gmail.com>
2020-10-07 15:49:00 +0530

    CodeChef October Challenge Chef and Easy Queries

---
+++ 74CHEFEZQ.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+
+void solve()
+{
+
+    ll n_queries;
+    ll k;
+    cin >> n_queries >> k;
+    ll arr[n_queries];
+    for (ll i = 0; i < n_queries; i++)
+    {
+        cin >> arr[i];
+    }
+    ll ans;
+    bool flag = 0;
+    ll i;
+    for (i = 0; i < n_queries; i++)
+    {
+        if (arr[i] < k)
+        {
+            flag = 1;
+            ans = i;
+            break;
+        }
+        else
+        {
+            arr[i + 1] += (arr[i] - k);
+        }
+    }
+    if (flag == 1)
+    {
+        cout << ans << endl;
+    }
+    else
+    {
+        ans = n_queries + (arr[n_queries - 1] / k);
+        cout << ans << endl;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        solve();
+    }
+    return 0;
+}
