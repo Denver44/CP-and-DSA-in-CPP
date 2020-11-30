29aabc2f05cea2a60bcc0ca24b9fb3b2df18f901
Durgesh <durgeshrai2012@gmail.com>
2020-11-30 20:27:29 +0530

    Codeforces Eduacational Round 99 Strange Functions

---
+++ 122STRANGEFUNCTIONS.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+
+void solve()
+{
+    string str;
+    cin >> str;
+    cout << str.size() << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cout << fixed;
+    cout << setprecision(10);
+    int t = 1;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        solve();
+    }
+    return 0;
+}
