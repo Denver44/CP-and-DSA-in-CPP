da14d83c012d53a7ec26a034e748d75239f1df38
Durgesh <durgeshrai2012@gmail.com>
2020-12-01 15:14:51 +0530

    Codeforces Round 99 Problem JUMPS

--- 123JUMPS.cpp
+++ 123JUMPS.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
-
+// simple question we have to see the pattern here
+//  we will move y till it get equal to destination or get greater than destination.
+// as if the y is just greater than destination by 1. then we have to just add 1 to jumps and that will be the answer.
+// as the y- dest diff is grater than one then we jhave to make some chnages in our jumps to reacher ther under that y
+// eg if dest is 16 and we move y 6 jumps and reach 21 -> 0 1 3  6  10 15 21
+//  then by subtracting y- dest -1 we get the jump name which we should not take in place we have t reverse back by -1.
+// 21 -16 = 5-1 4 jumps should not be taken
+// 0 -st Jump-> 1 -2nd Jump-> 3 -3rd Jump ->6 -> 4 jump -1 so we reach 5 - 5th Jump -> 10 -6 th Jump ->16
 
 void solve()
 {
