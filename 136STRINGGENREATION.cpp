c86f4ddd79d527cd5731069cde47a6c8d62c3b87
Durgesh <durgeshrai2012@gmail.com>
2020-12-13 17:32:42 +0530

    Codeforces Problem StringGeneartion DIV2 Zen Code

--- 136STRINGGENREATION.cpp
+++ 136STRINGGENREATION.cpp
@@ -35,3 +35,51 @@ int main()
     }
     return 0;
 }
+
+// --------------- MY APPROACH ----------
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n, k;
+        cin >> n >> k;
+        char a[n];
+        a[0] = 'a';
+        int j = 1;
+        for (j = 1; j <= n - k; j++)
+        {
+            if (a[j - 1] == 'a')
+                a[j] = 'b';
+            else if (a[j - 1] == 'b')
+                a[j] = 'c';
+            else
+            {
+                a[j] = 'a';
+            }
+        }
+        int run = k - 1;
+        while (run--)
+        {
+            if (a[j - 1] == 'a')
+                a[j] = 'a';
+            else if (a[j - 1] == 'b')
+                a[j] = 'b';
+            else
+            {
+                a[j] = 'c';
+            }
+            j++;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cout << a[i];
+        }
+        cout << endl;
+    }
+}
