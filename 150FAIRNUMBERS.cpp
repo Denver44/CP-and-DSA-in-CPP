f3f16e17eb3aa804f567bc98e858ba4bcdc19121
Durgesh <durgeshrai2012@gmail.com>
2020-12-21 10:01:35 +0530

    Codeforces Problem FairGame TechnoCup

---
+++ 150FAIRNUMBERS.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long ull;
+
+void file_i_o()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+void solve()
+{
+	ull n;
+	cin >> n;
+	while (true)
+	{
+		ull a = n;
+		bool flag1 = false;
+		while (a)
+		{
+
+			ull rem = (a % 10);
+			if (rem == 0)
+			{
+				a = a / 10;
+				continue;
+			}
+			if (n % rem != 0)
+			{
+				flag1 = true;
+				break;
+			}
+			else
+				a /= 10;
+		}
+		if (flag1)
+		{
+			n++;
+			a = n;
+		}
+		else
+		{
+			cout << n << endl;
+			break;
+		}
+	}
+}
+
+int main()
+{
+	file_i_o();
+	int t = 1;
+	cin >> t;
+	for (int i = 0; i < t; i++)
+	{
+		solve();
+	}
+
+	return 0;
+}
