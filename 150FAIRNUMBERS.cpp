0f2a65ab7a77179d5e89640a241367c5e29f7122
Durgesh <durgeshrai2012@gmail.com>
2020-12-21 10:33:30 +0530

    CodeForces Problem FairNumbers TechnoCup

--- 150FAIRNUMBERS.cpp
+++ 150FAIRNUMBERS.cpp
@@ -8,8 +8,11 @@ void file_i_o()
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
 }
-
 void solve()
 {
 	ull n;
