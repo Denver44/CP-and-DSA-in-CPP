3838a81abcf9bcf13be524fdae19f440bf84f4b7
Durgesh <durgeshrai2012@gmail.com>
2021-01-11 15:22:28 +0530

    CodeForces Problem Round 693 Div3 EvenOddGame

--- 184EVENODDGAME.cpp
+++ 184EVENODDGAME.cpp
@@ -16,42 +16,38 @@ void file_i_o()
 	freopen("output.txt", "w", stdout);
 #endif
 }
+// Here we as Alice always Moves first so if there is not even number who is Highest in the array he will pick the odd number which is highest int array so bob it dint get that point.
+// So using this startergy we sort the array first and Alice picks first if ist even then add to ans else no one get points So wheb Bob Chance Come it will subtract that number from global ans.
+// If the GLobal Answer is Zero it means it is Tie.
+// If its Negative means Bobs Wins
+// If its Positive means Alice win
 
 void solve()
 {
 
-	int n;
+	ll n, ans = 0;
 	cin >> n;
-	int sumeven = 0, sumodd = 0;
 	vector<ll> arr(n);
 	for (auto &i : arr)
-	{
 		cin >> i;
-	}
 	sort(arr.rbegin(), arr.rend());
-	int player = 1;
 	for (int i = 0; i < arr.size(); ++i)
 	{
-		// log(player)
-		if (player)
+		if (i % 2 == 0)
 		{
 			if (arr[i] % 2 == 0)
-				sumeven += arr[i];
+				ans += arr[i];
 		}
 		else
 		{	if (arr[i] % 2 == 1)
-				sumodd += arr[i];
+				ans -= arr[i];
 		}
-		player ^= 1;
 	}
-	log(sumeven)
-	log(sumodd)
-
-	if (sumeven > sumodd)
+	if (ans > 0)
 	{
 		log("Alice");
 	}
-	else if (sumeven < sumodd)
+	else if (ans < 0)
 	{
 		log("Bob");
 	}
@@ -72,48 +68,3 @@ int main()
 }
 
 
-
-
-// void solve()
-// {
-
-// 	int n;
-// 	cin >> n;
-// 	int counteven = 0, countodd = 0;
-// 	int sumeven = 0, sumodd = 0;
-// 	for (int i = 0; i < n; ++i)
-// 	{
-// 		int b; cin >> b;
-// 		if (b % 2 == 0)
-// 		{	counteven++;
-// 			sumeven += b;
-// 		}
-// 		else {
-// 			countodd++;
-// 			sumodd += b;
-// 		}
-// 	}
-// 	if (counteven == 0 && countodd >= 2)
-// 	{
-// 		log("Bob");
-// 	}
-// 	else if (( counteven < countodd) && (countodd - counteven) >= 2  && sumeven < sumodd)
-// 	{
-// 		log("Bob");
-// 	}
-// 	else if ( counteven == countodd && sumeven <= sumodd  )
-// 	{
-// 		log("Tie");
-// 	}
-// 	else if ( (counteven < countodd) && (sumeven < sumodd) && ((countodd - counteven ) == 1) )
-// 	{
-// 		log("Tie");
-// 	}
-// 	else if (countodd == 0 && counteven >= 1)
-// 	{
-// 		log("Alice")
-// 	}
-// 	else
-// 		log("Alice");
-
-// }
