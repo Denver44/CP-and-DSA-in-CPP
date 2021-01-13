66a4a0ea587c1091a0ba75c14e6b91f527cf9c33
Durgesh <durgeshrai2012@gmail.com>
2021-01-13 18:42:52 +0530

    Codechef Problem JAN21 WIPL Recursion+Memoization

--- 181WIPL.cpp
+++ 181WIPL.cpp
@@ -16,82 +16,27 @@ void file_i_o()
 	freopen("output.txt", "w", stdout);
 #endif
 }
-void helper1(int h, vector<int> & arr)
+
+vector<vector<int>> dp;
+
+int solve( vector<int> & arr , int height, int h1, int h2 , int i)
 {
-
-	int index = 0, fTower = 0, count  = 0;
-	while (fTower < h  && index < arr.size() )
+	int n =  arr.size();
+	if (h1 >= height)
 	{
-		int t = -1;
-		fTower += arr[index];
-		if (fTower >= h ) {
-			index++;
-			break;
+		if (h2 >= height) {
+			return i;
 		}
-		int j = index + 1;
-		while (arr[j] >= (h - fTower)  && j < arr.size())
-		{
-			t = 1;
-			j++;
-		}
-		if (t == 1)
-		{	swap(arr[index + 1], arr[--j]);
-			fTower += arr[index + 1];
-		}
-		else
-			fTower += arr[index + 1];
-		index += 2;
+		return 1e9;
+	}
+	if (i >= n) {
+		return 1e9;
+	}
+	if (dp[i][h1] != -1) {
+		return dp[i][h1];
 	}
 
-	int sTower = 0;
-	while ( sTower < h && index < arr.size() )
-	{
-		int t = -1;
-		sTower += arr[index];
-		if ( sTower >= h ) {
-			index++;
-			break;
-		}
-		int j = index + 1;
-		while (arr[j] >= (h - sTower) && j < arr.size())
-		{
-			t = 1;
-			j++;
-		}
-		if (t == 1)
-		{	swap(arr[index + 1], arr[--j]);
-			sTower += arr[index + 1];
-		}
-		else
-		{sTower += arr[index + 1];}
-
-		index += 2;
-	}
-	log(index);
-}
-
-bool helper(int target, vector<int> & arr)
-{
-	vector<vector<bool>>dp (arr.size() + 1, vector<bool>(target + 1, false));
-	for (int i = 0; i < dp.size(); ++i)
-		dp[i][0] = true;
-
-	int n = arr.size();
-	for (int i = 1; i <= n ; ++i)
-	{
-		for (int j = 1; j <= target; ++j)
-		{
-			if (j >= arr[i - 1])
-			{
-				dp[i][j] = (dp[i - 1][j] || dp[i - 1][j - arr[i - 1]]);
-			}
-			else
-			{
-				dp[i][j] = dp[i - 1][j];
-			}
-		}
-	}
-	return dp[n][target];
+	return dp[i][h1] =  min(solve(arr, height, h1 + arr[i] , h2 , i + 1) , solve(arr, height, h1 , h2 + arr[i], i + 1));
 }
 
 void solve()
@@ -99,55 +44,17 @@ void solve()
 	int n, h;
 	cin >> n >> h;
 	vector<int> arr(n);
-	for (auto &i : arr)
-		cin >> i;
-	int sum = accumulate(arr.begin(), arr.end() , 0);
-	sort(arr.rbegin(), arr.rend());
-	if (sum < (h * 2))
-	{	log(-1)
-		return;
-	}
-	else if ( arr[0] > h)
-	{
-		helper1( h, arr);
-		return;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
 
+	sort(arr.begin(), arr.end(), greater<int>());
+	dp.assign(n + 1, vector<int>( h + 10, -1));
+	int res = solve(arr, h, 0, 0, 0);
+	if ( res == 1e9) {
+		log(-1)
 	}
-	else
-	{
-		int index = 0;
-		int sum = 0;
-		vector<int> arr1;
-		while (index < n && sum < (h * 2))
-		{
-			arr1.push_back(arr[index]);
-			sum += arr[index++];
-		}
-		// log(sum);
-		bool flag1 = helper(h , arr1);
-		while (index < n)
-		{
-			if (flag1 == true) {
-				log(index);
-				return;
-			}
-			else if (flag1 == false)
-			{
-				// log("Hello")
-				arr1.push_back(arr[index++]);
-				int i;
-				loop( i, 0, arr1.size())
-				log(arr1[i]);
-				flag1 = helper(h , arr1);
-			}
-
-		}
-		if (flag1 == false) {
-			log(-1);
-		}
-		else {
-			log(index);
-		}
+	else {
+		log(res)
 	}
 
 }
