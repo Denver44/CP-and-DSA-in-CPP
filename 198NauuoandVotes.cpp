91601d1b39af26ac0fd9bc2620a83b723d96a900
Durgesh <durgeshrai2012@gmail.com>
2021-01-15 22:01:35 +0530

    Codeforces Round #564 (Div. 2) Nauuo and Votes

---
+++ 198NauuoandVotes.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef  long long ll;
+typedef unsigned long long ull;
+#define log(a) cout<<(a)<<endl;
+#define v vector<int>
+#define loop(i,a,b) for(ll i = a; i < b; i++)
+#define looprev(i,a,b) for(ll i = a-1; i >=b; i--)
+void file_i_o()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve()
+{
+	int up, down, middle;
+	cin >> up >> down >> middle;
+
+	if (up > down + middle) {
+		log("+");
+	}
+	else if (down > up + middle) {
+		log("-");
+	}
+	else if (up == down  && middle == 0) {
+		log("0");
+	}
+	else {
+		log("?");
+	}
+}
+
+int main()
+{
+	file_i_o();
+	int t = 1, i = 0;
+	// cin >> t;
+	loop (i, 0, t)
+	solve();
+	return 0;
+}
+
+
+
