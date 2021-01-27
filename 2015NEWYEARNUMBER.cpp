2a517e70f05bcccd15467ae2ba9e2b685faff736
Durgesh <durgeshrai2012@gmail.com>
2021-01-27 16:55:29 +0530

    Codeforces Problem Round 697 Div 3  New year Number

---
+++ 2015NEWYEARNUMBER.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef  long long ll;
+typedef unsigned long long ull;
+#define log(a) cout<<(a)<<endl;
+#define v vector<int>
+#define loop(i,a,b) for(ll i = a; i < b; i++)
+#define looprev(i,a,b) for(ll i = a-1; i >=b; i--)
+void file_i_o()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+// Here we have to check that can we make the givern n as certain 2020 + certain 2021.
+
+void solve()
+{
+	int n;
+	cin >> n;
+
+	int req = n / 2020;
+	if ( (n % 2020) <= req )
+	{
+		log("YES")
+	}
+	else
+	{
+		log("NO")
+	}
+
+}
+
+
+int main()
+{
+	file_i_o();
+
+	int t = 1, i = 0;
+	cin >> t;
+	loop (i, 0, t)
+	solve();
+	return 0;
+}
+
