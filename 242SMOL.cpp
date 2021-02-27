44b0299eae5c38d387fe64df69818124a9396c3f
Durgesh <durgeshrai2012@gmail.com>
2021-02-27 19:58:53 +0530

    Codechef Feb21 LunchTime Problem SMOL

---
+++ 242SMOL.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef  long long ll;
+typedef unsigned long long ull;
+#define log(a) cout<<(a)<<endl;
+#define v vector<int>
+#define loop(i,a,b) for(ll i = a; i < b; i++)
+#define looprev(i,a,b) for(ll i = a-1; i >=b; i--)
+void file_i_o()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+void solve()
+{
+	ll n , k ;
+	cin >> n >> k;
+	if (k == 0) {
+		log(n);
+		return;
+	}
+	ll ans = n % k;
+	log(ans);
+
+}
+
+int main()
+{
+	file_i_o();
+	int t = 1;
+	cin >> t;
+	for (int i = 0; i < t; i++)
+	{
+		solve();
+	}
+	return 0;
+}
